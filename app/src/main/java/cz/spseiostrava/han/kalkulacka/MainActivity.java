d855606d93ed94276d6a8c0b3770a4166f70fc9a
Petr Hanzlik <p.hanzlik.st@spseiostrava.cz>
2024-09-30 10:25:11 +0200

    logika scitani

--- app/src/main/java/cz/spseiostrava/han/kalkulacka/MainActivity.java
+++ app/src/main/java/cz/spseiostrava/han/kalkulacka/MainActivity.java
@@ -3,12 +3,34 @@ package cz.spseiostrava.han.kalkulacka;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText number1, number2;
+    Button button;
+    TextView vylsedek;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        number1 = findViewById(R.id.number1);
+        number2 = findViewById(R.id.number2);
+        button = findViewById(R.id.button);
+        vylsedek = findViewById(R.id.vysledek);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int vysledek = Integer.parseInt(number1.getText().toString()) + Integer.parseInt(number2.getText().toString());
+                vylsedek.setText(String.valueOf(vysledek));
+            }
+        });
+
     }
 }

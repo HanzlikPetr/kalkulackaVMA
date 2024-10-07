747d353db6608969f1e03fadccea292ab499da3d
Petr Hanzlik <p.hanzlik.st@spseiostrava.cz>
2024-10-07 10:27:58 +0200

    přidaní nových operací

--- app/src/main/java/cz/spseiostrava/han/kalkulacka/MainActivity.java
+++ app/src/main/java/cz/spseiostrava/han/kalkulacka/MainActivity.java
@@ -7,11 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText number1, number2;
-    Button button;
+    Button buttonSoucet, buttonRozdil, buttonSoucin, buttonPodil;
     TextView vylsedek;
 
     @Override
@@ -21,16 +22,44 @@ public class MainActivity extends AppCompatActivity {
 
         number1 = findViewById(R.id.number1);
         number2 = findViewById(R.id.number2);
-        button = findViewById(R.id.button);
         vylsedek = findViewById(R.id.vysledek);
+        buttonSoucet = findViewById(R.id.buttonSoucet);
+        buttonRozdil = findViewById(R.id.buttonRozdil);
+        buttonPodil = findViewById(R.id.buttonPodil);
+        buttonSoucin = findViewById(R.id.buttonSoucin);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        View.OnClickListener tlacitkaListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!number1.getText().toString().equals("") && !number2.getText().toString().equals(""))
-                    vylsedek.setText((Double.parseDouble(number1.getText().toString()) +  Double.parseDouble(number2.getText().toString())) + "");
+                int btnId = view.getId();
+                double cislo1, cislo2, vysledekPom;
+
+                if(!number1.getText().toString().equals("") && !number2.getText().toString().equals("")){
+                    cislo1 = Double.parseDouble(number1.getText().toString());
+                    cislo2 = Double.parseDouble(number2.getText().toString());
+
+                    if(btnId == R.id.buttonSoucet){
+                        vysledekPom = cislo1 + cislo2;
+                    } else if (btnId == R.id.buttonRozdil) {
+                        vysledekPom = cislo1 - cislo2;
+                    } else if (btnId == R.id.buttonSoucin) {
+                        vysledekPom = cislo1 * cislo2;
+                    }else{
+                        vysledekPom = cislo1 / cislo2;
+                    }
+
+                    vylsedek.setText(String.valueOf(vysledekPom));
+                }else{
+                    Toast.makeText(getApplicationContext() , "Zadej 2 hodnoty", Toast.LENGTH_SHORT).show();
+                }
             }
-        });
+        };
+
+        buttonSoucet.setOnClickListener(tlacitkaListener);
+        buttonRozdil.setOnClickListener(tlacitkaListener);
+        buttonSoucin.setOnClickListener(tlacitkaListener);
+        buttonPodil.setOnClickListener(tlacitkaListener);
+
 
     }
 }
